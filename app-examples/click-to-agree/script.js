369f58bccc4585a5a9d8ad0cc5f92291f896bd7b
Larry Kluger <larry.kluger@docusign.com>
2024-03-31 00:03:54 +0300

    add complete login spinner feedback

--- app-examples/click-to-agree/script.js
+++ app-examples/click-to-agree/script.js
@@ -2,6 +2,12 @@ import {
     ImplicitGrant,
 } from "../library/implicitGrant.js" 
 
+import {
+    CallApi,
+    UserInfo
+} from "../library/callapi.js" 
+
+
 import { CheckTemplates 
 } from "../library/checkTemplates.js";
 
@@ -16,17 +22,37 @@ import {
 
 const CLIENT_ID = "demo";
 
-$(function () {
+$(async function () {
     // #####################################################
     const logLevel = 0; // 0 is terse; 9 is verbose
     // Change the log level to see the requests / responses
     // #####################################################
 
+    let sign = function signF () {
 
+
+    }.bind(this)
 
     let login = function loginF() {
         data.implicitGrant.login();
     }.bind(this);
+
+    async function completeLogin() {
+        data.userInfo = new UserInfo({
+            accessToken: data.implicitGrant.accessToken,
+        });
+        const ok = await data.userInfo.getUserInfo();
+        if (ok) {
+            data.callApi = new CallApi({
+                accessToken: data.implicitGrant.accessToken,
+                apiBaseUrl: data.userInfo.defaultBaseUrl
+            });
+        } else {
+            // Did not complete the login
+            toast(data.userInfo.errMsg);
+        }
+        return ok;
+    }
 
     // Mainline
     let data = {
@@ -46,14 +72,17 @@ $(function () {
         const loginModal = new bootstrap.Modal('#modalLogin');
         loginModal.show();
     } else {
+        // logged in
+        const loadingModal = new bootstrap.Modal('#loading');
+        loadingModal.show();
+        await completeLogin();
+        loadingModal.hide();
         toast("Logged In!")
     }
 
-
     //window.addEventListener("message", messageListener);
     $("#modalLogin button").click(login);
-    $("#btnDoit").click(doit);
-    $("#chooseFile").change(chooseFile);
+    $("#sign").click(sign);
     const smsEl = document.querySelector("#sms");
 
 })
